7d256754cd8ab07212664ca2eb0f1a1c0987a47c
Phil Lee <prodigy7748g@gmail.com>
2021-03-23 15:04:52 +0800

    jwt authentication setting for devise

--- app/models/json_web_token.rb
+++ app/models/json_web_token.rb
@@ -1,9 +1,11 @@
 class JsonWebToken
   def self.encode(payload)
-    JWT.encode payload, Rails.application.secrets.secret_key_base
+    expiration = 30.minutes.from_now.to_i
+    JWT.encode payload.merge(exp: expiration), Rails.application.secrets.secret_key_base
   end
 
   def self.decode(token)
     JWT.decode(token, Rails.application.secrets.secret_key_base).first
   end
 end
+ 
